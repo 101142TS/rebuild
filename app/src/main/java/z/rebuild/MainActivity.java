32c587bfd9c6c4d8f62101e4957bf94c86a2716d
exia <exia@pku.com>
2019-11-26 10:29:59 +0800

    11.26

--- app/src/main/java/z/rebuild/MainActivity.java
+++ app/src/main/java/z/rebuild/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super();
     }
 
+    //public final static String mTargetPackage = "com.example.simple";
     //public final static String mTargetPackage = "com.vjson.animde";  //legu 2.10.2.2
     //public final static String mTargetPackage = "com.jr.kingofglorysupport"; //legu 2.10.2.3
     //public final static String mTargetPackage = "com.billy.sdclean"; //2.10.4.0
@@ -55,10 +56,37 @@ public class MainActivity extends AppCompatActivity {
     //public final static String mTargetPackage = "io.github.hopedia";
     //public final static String mTargetPackage = "org.schabi.newpipelegacy";
     //public final static String mTargetPackage = "eu.depau.etchdroid";
-    public final static String mTargetPackage = "com.qihoo360.mobilesafe.opti.powerctl";
+    //public final static String mTargetPackage = "com.qihoo360.mobilesafe.opti.powerctl";
     //public final static String mTargetPackage = "com.forever.browser";
     //public final static String mTargetPackage = "com.softbank.mbank.xy.qhjd";
     //public final static String mTargetPackage = "com.picc.aasipods";
+    //public final static String mTargetPackage = "com.showself.ui";
+    //public final static String mTargetPackage = "com.huaqian";
+    //public final static String mTargetPackage = "com.zlqb.app";
+    //public final static String mTargetPackage = "com.storm.smart";
+    //public final static String mTargetPackage = "cn.xxt.jxlxandroid";
+    //public final static String mTargetPackage = "com.esbook.reader";
+    //public final static String mTargetPackage = "cn.xxt.jxlxandroid";
+    //public final static String mTargetPackage = "com.anysoft.tyyd";
+    //public final static String mTargetPackage = "com.autohome.mycar";
+    //public final static String mTargetPackage = "com.wenba.bangbang";
+    //public final static String mTargetPackage = "fm.jihua.kecheng";
+    //public final static String mTargetPackage = "com.qihoo.loan";
+    //public final static String mTargetPackage = "com.ludashi.benchmark";
+    //public final static String mTargetPackage = "com.shcc.microcredit";
+    //public final static String mTargetPackage = "com.mobike.mobikeapp";
+    //public final static String mTargetPackage = "com.pmp.ppmoney";
+    //public final static String mTargetPackage = "com.bertadata.qxb";
+    //public final static String mTargetPackage = "org.cocos2dx.VirusVsVirus2";
+    //public final static String mTargetPackage = "com.memezhibo.android";
+    //public final static String mTargetPackage = "com.mojang.minecraftpetool";
+    //public final static String mTargetPackage = "com.tencent.qlauncher.lite";
+    //public final static String mTargetPackage = "com.tencent.tgclub";
+    //public final static String mTargetPackage = "com.black.unique";
+    //public final static String mTargetPackage = "com.fqapp.zsh";
+    //public final static String mTargetPackage = "com.songguoyhqw";
+    //public final static String mTargetPackage = "com.sf.activity";
+    public final static String mTargetPackage = "com.shangfang.gylm";
     static {
         System.loadLibrary("rebuild");
     }
